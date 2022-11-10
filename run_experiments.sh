7e160e50dd557f24d1919a2dc123f3939d1c7ce3
damifur <damian.a.furman@gmail.com>
2022-11-10 19:46:02 -0300

    Partial results

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,11 +2,13 @@ for complexity in simple complex
 do
     for program_size in 200 500 1000
     do
-        for lr in 2e-03 5e-03 5e-02
+        for lr in 5e-02 1e-02
         do
 	    for layer_size in 300
             do
-		    python train_nn.py --lr ${lr} --num_layers 10 --complexity ${complexity} --program_size ${program_size} --layers_size ${layer_size} --blocking True --output_size 3
+		    python train_nn.py --lr ${lr} --num_layers 10 --complexity ${complexity} --program_size ${program_size} --layers_size ${layer_size} --blocking True --output_size 3 --epochs 8
+		    python train_nn.py --lr ${lr} --num_layers 10 --complexity ${complexity} --program_size ${program_size} --layers_size ${layer_size} --epochs 8
+		    
             done
 	done
     done
